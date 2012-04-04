bace3a0a80e3d47ebb6487ed23699a37ed3985e2
Michael McGlynn <micmcg@gmail.com>
2012-04-04 10:03:40 +1000

    Fixed profile binding for FF 7+

--- ffv.sh
+++ ffv.sh
@@ -21,8 +21,8 @@ for i in "${versions[@]}"; do
 
     echo "  # Creating profile for Firefox-$i #"
     /Applications/Firefox-Versions/Firefox-$i.app/Contents/MacOS/firefox-bin -CreateProfile Firefox-$i
-    mv /Applications/Firefox-Versions/Firefox-$i.app/Contents/MacOS/firefox-bin /Applications/Firefox-Versions/Firefox-$i.app/Contents/MacOS/firefox-bin-orig
 
+    echo ""
     echo "  # Disabling auto update and default browser check for Firefox-$i #"
     find ~/Library/Application\ Support/Firefox/Profiles/*.Firefox-$i -name prefs.js -print0 -exec sh -c 'echo "
     user_pref(\"app.update.auto\", false);
@@ -32,9 +32,12 @@ for i in "${versions[@]}"; do
     user_pref(\"browser.rights.3.shown\", true);" >> $1' -- {} \;
 
     echo "  # Binding profile for Firefox-$i #"
+    mv /Applications/Firefox-Versions/Firefox-$i.app/Contents/MacOS/firefox-bin /Applications/Firefox-Versions/Firefox-$i.app/Contents/MacOS/firefox-bin-orig
+    mv /Applications/Firefox-Versions/Firefox-$i.app/Contents/MacOS/firefox /Applications/Firefox-Versions/Firefox-$i.app/Contents/MacOS/firefox-orig
     echo "#!/bin/bash
     exec \"/Applications/Firefox-Versions/Firefox-$i.app/Contents/MacOS/firefox-bin-orig\" -P Firefox-$i" > /Applications/Firefox-Versions/Firefox-$i.app/Contents/MacOS/firefox-bin
     chmod +x /Applications/Firefox-Versions/Firefox-$i.app/Contents/MacOS/firefox-bin
+    ln -s /Applications/Firefox-Versions/Firefox-$i.app/Contents/MacOS/firefox-bin /Applications/Firefox-Versions/Firefox-$i.app/Contents/MacOS/firefox
 
     echo "### FINISHED INSTALLING FIREFOX $i ###"
     echo ""
